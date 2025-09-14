f799f47abe1a1fbb68168fa4cd5ce1c364650d21
Artur Rusinov <cerberos1940@gmail.com>
2025-09-15 01:17:12 +0300

    синхронизация с docker, timestamp создания user/product

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,8 @@ ENV PYTHONUNBUFFERED=1
 
 COPY requirements.txt .
 RUN python -m pip install -r requirements.txt && \
-    python -m pip install --upgrade pip
+    python -m pip install --upgrade pip && \
+    apt-get update && apt-get install -y sqlite3
 
 WORKDIR /bot_app
 
--- database/sqlite_db.py
+++ database/sqlite_db.py
@@ -1,6 +1,16 @@
 from datetime import datetime
 
-from sqlalchemy import Column, DateTime, Float, Integer, String, Text, select, text
+from sqlalchemy import (
+    Column,
+    DateTime,
+    Float,
+    Integer,
+    String,
+    Text,
+    select,
+    text,
+    func,
+)
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import declarative_base
 
@@ -14,21 +24,32 @@ class User(Base):
     username = Column(String(100))
     first_name = Column(String(100))
     last_name = Column(String(100), nullable=True)
-    created_at = Column(DateTime, default=datetime.now)
+    created_at = Column(
+        DateTime,
+        server_default=text("CURRENT_TIMESTAMP"),
+        default=func.now(),
+        nullable=False,
+    )
 
 
 class Product(Base):
     __tablename__ = "products"
     id = Column(Integer, primary_key=True)
     name = Column(String(255), nullable=False)
+    size = Column(String(50), nullable=True)
     description = Column(Text, nullable=True)
     price = Column(Float, nullable=False)
     category = Column(String(100), nullable=True)
-    created_at = Column(DateTime, default=datetime.now)
+    created_at = Column(
+        DateTime,
+        server_default=text("CURRENT_TIMESTAMP"),
+        default=func.now(),
+        nullable=False,
+    )
 
 
 class AsyncSQLiteDatabase:
-    def __init__(self, db_path: str = "app/database/shop.db"):
+    def __init__(self, db_path: str = "database/shop.db"):
         import os
 
         os.makedirs(os.path.dirname(db_path), exist_ok=True)
@@ -40,10 +61,8 @@ class AsyncSQLiteDatabase:
     async def init_db(self):
         async with self.engine.begin() as conn:
             await conn.run_sync(Base.metadata.create_all)
-        print(f"Была создана AsyncSQLite база данных: app/database/shop.db")
+        print(f"Была создана AsyncSQLite база данных: database/shop.db")
         return self
-
-
 
     async def add_user(
         self, user_id: int, username: str, first_name: str, last_name: str | None
@@ -63,10 +82,11 @@ class AsyncSQLiteDatabase:
             result = await session.execute(select(Product))
             return result.scalars().all()
 
-
-    async def get_product(self, product_id:int):
+    async def get_product(self, product_id: int):
         async with self.AsyncSession() as session:
-            result = await session.execute(select(Product).where(Product.id == product_id))
+            result = await session.execute(
+                select(Product).where(Product.id == product_id)
+            )
             return result.scalar()
 
     async def check_connection(self):
@@ -77,7 +97,6 @@ class AsyncSQLiteDatabase:
         except Exception as e:
             print(f"❌ Ошибка подключения к БД: {e}")
             return False
-            
 
 
 async def init_async_sqlite():
